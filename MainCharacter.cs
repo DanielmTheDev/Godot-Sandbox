a06cb75b4f4704efe82d72ad4e2cd62bfa0730ad
Daniel Muckelbauer <daniel.muckelbauer@sinc.de>
2025-06-18 11:58:45 +0200

    movementi

--- MainCharacter.cs
+++ MainCharacter.cs
@@ -9,9 +9,29 @@ public partial class MainCharacter : CharacterBody2D
 
     public override void _PhysicsProcess(double delta)
     {
-        var x = Input.IsActionPressed("move_right") ? 1 : Input.IsActionPressed("move_left") ? -1 : 0;
-        var y = Velocity.Y + (!IsOnFloor() ? Gravity * (float)delta : 0);
-        Velocity = new Vector2(x * MoveSpeed, y);
+        SetVelocity(delta);
         MoveAndSlide();
+    }
+
+    private void SetVelocity(double delta)
+    {
+        var x = GetXMovement();
+        var y = GetYMovement(delta);
+        Velocity = new Vector2(x, y);
+    }
+
+    private float GetYMovement(double delta)
+        => Velocity.Y + (!IsOnFloor()
+            ? Gravity * (float)delta
+            : 0);
+
+    private static float GetXMovement()
+    {
+        var raw = Input.IsActionPressed("move_right")
+            ? 1
+            : Input.IsActionPressed("move_left")
+                ? -1
+                : 0;
+        return raw * MoveSpeed;
     }
 }
