f0465876dfeb13ea34dbff48f79db1489470ab3a
Daniel Muckelbauer <daniel.muckelbauer@sinc.de>
2025-06-22 06:14:59 +0200

    blood

--- Players/Scripts/TakingDamage.cs
+++ Players/Scripts/TakingDamage.cs
@@ -5,6 +5,13 @@ namespace Sandbox.Players.Scripts;
 public partial class TakingDamage : Node
 {
     [Export] public MainCharacter MainCharacter = null!;
+    private GpuParticles2D _bloodEmitter = null!;
 
-    public void GetHit(Area2D area) => MainCharacter.SwitchState(States.StateName.Dead);
+    public override void _Ready() => _bloodEmitter = GetNode<GpuParticles2D>("../BloodEmitter");
+
+    public void GetHit(Area2D area)
+    {
+        MainCharacter.SwitchState(States.StateName.Dead);
+        _bloodEmitter.Restart();
+    }
 }
