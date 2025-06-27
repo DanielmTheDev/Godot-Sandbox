22d556c495d6f50c362016fc94ce2d71d04a4dbd
Daniel Muckelbauer <dadamuck@gmail.com>
2025-06-27 21:51:36 +0200

    The one stuff

--- Players/Scripts/Gravity.cs
+++ Players/Scripts/Gravity.cs
@@ -4,14 +4,14 @@ namespace Sandbox.Players.Scripts;
 
 public partial class Gravity : Node
 {
-    [Export] public MainCharacter MainCharacter = null!;
+	[Export] public MainCharacter MainCharacter = null!;
 
-    private const float GravityForce = 1000f;
+	private const float GravityForce = 1000f;
 
-    public override void _PhysicsProcess(double delta)
-    {
-        var force = MainCharacter.Velocity.Y + GravityForce * (float)delta;
-        MainCharacter.Velocity = new Vector2(MainCharacter.Velocity.X, force);
-        MainCharacter.MoveAndSlide();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		var force = MainCharacter.Velocity.Y + GravityForce * (float)delta;
+		MainCharacter.Velocity = new Vector2(MainCharacter.Velocity.X, force);
+		MainCharacter.MoveAndSlide();
+	}
 }
--- Players/Scripts/TakingDamage.cs
+++ Players/Scripts/TakingDamage.cs
@@ -7,11 +7,11 @@ public partial class TakingDamage : Node
     [Export] public MainCharacter MainCharacter = null!;
     private GpuParticles2D _bloodEmitter = null!;
 
-    public override void _Ready() => _bloodEmitter = GetNode<GpuParticles2D>("../BloodEmitter");
+    public override void _Ready() => _bloodEmitter = GetNode<GpuParticles2D>("../../BloodEmitter");
 
     public void GetHit(Area2D area)
     {
-        MainCharacter.SwitchState(States.StateName.Dead);
         _bloodEmitter.Restart();
+        MainCharacter.SwitchState(States.StateName.Dead);
     }
 }
